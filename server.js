2532e718423494f1b2cad856d7791476ee18da6d
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2018-05-20 01:15:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ console.log("process.env.OPENSHIFT_NODEJS_IP : "+process.env.OPENSHIFT_NODEJS_IP
 console.log("process.env.OPENSHIFT_NODEJS_PORT : "+process.env.OPENSHIFT_NODEJS_PORT );
 var server_port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP ||process.env.IP|| '127.0.0.1';
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 server.connection({
 	host: ip,
