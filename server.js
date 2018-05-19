ba1499cc0d52ce7eccbeb3b8389760a1052899ac
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2018-05-20 00:12:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
-
+console.log("Hapi got");
 server.connection({
 	host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 });
 
 server.register([require('inert'), require('hapi-error')], function () {
-
+console.log("Hapi Ready");
 	server.route([
 	  { method: 'GET', path: '/', handler: { file: "index.html" } },
 		// switch these two routes for a /static handler?
 	  { method: 'GET', path: '/client.js', handler: { file: './lib/client.js' } },
 		{ method: 'GET', path: '/style.css', handler: { file: './css/style.css' } },
-	  { method: 'GET', path: '/bootstrap.min.css', handler: { file: './css/bootstrap.min.css' } },
-	  { method: 'GET', path: '/load',      handler: require('./lib/load_messages').load }
+	  { method: 'GET', path: '/bootstrap.min.css', handler: { file: './css/bootstrap.min.css' } }
 	]);
 
 	server.start(function () {
@@ -22,6 +21,7 @@ server.register([require('inert'), require('hapi-error')], function () {
 			// console.log('REDISCLOUD_URL:', process.env.REDISCLOUD_URL);
 			console.log('Feeling Chatty?', 'listening on: http://127.0.0.1:' + 8000);
 		});
+		console.log("Hapi server start");
 	});
 
 });
