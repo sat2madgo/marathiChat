240aa637521ef7893ab5d5c279795e7944d2ebad
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2018-05-20 00:38:16 +0530

    added port logs

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
-console.log("Hapi got");
+console.log("process.env.IP : "+process.env.IP );
+console.log("process.env.PORT : "+process.env.PORT );
+console.log("process.env.OPENSHIFT_NODEJS_IP : "+process.env.OPENSHIFT_NODEJS_IP );
+console.log("process.env.OPENSHIFT_NODEJS_PORT : "+process.env.OPENSHIFT_NODEJS_PORT );
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 server.connection({
-	host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+	host: server_ip_address,
+	port: server_port
 });
 
 server.register([require('inert'), require('hapi-error')], function () {
@@ -21,7 +26,7 @@ console.log("Hapi Ready");
 			// console.log('REDISCLOUD_URL:', process.env.REDISCLOUD_URL);
 			console.log('Feeling Chatty?', 'listening on: http://127.0.0.1:' + 8000);
 		});
-		console.log("Hapi server start");
+		
 	});
 
 });
