10c0e513d8eba521f9600815def2a9b446ef41fd
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2018-05-20 01:29:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ server.register([require('inert'), require('hapi-error')], function () {
 
 	server.start(function () {
 		console.log('Server running on http://%s:%s', ip, port);
-		require('lib/chat').init(server.listener, function(){
+		require('./lib/chat').init(server.listener, function(){
 			// console.log('REDISCLOUD_URL:', process.env.REDISCLOUD_URL);
 			console.log('Feeling Chatty?', 'listening on: http://127.0.0.1:' + 8000);
 		});
