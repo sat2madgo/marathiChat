e8a784a4ad652187969383dff885536988897041
Shreyas Ajay Tigdi <sat2madgo@gmail.com>
2018-05-20 01:00:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ console.log("process.env.IP : "+process.env.IP );
 console.log("process.env.PORT : "+process.env.PORT );
 console.log("process.env.OPENSHIFT_NODEJS_IP : "+process.env.OPENSHIFT_NODEJS_IP );
 console.log("process.env.OPENSHIFT_NODEJS_PORT : "+process.env.OPENSHIFT_NODEJS_PORT );
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP ||process.env.IP|| '127.0.0.1';
 server.connection({
 	host: server_ip_address,
 	port: server_port
 });
-
+console.log("IP :"+server_ip_address);
+console.log("port :"+server_port);
+debugger;
 server.register([require('inert'), require('hapi-error')], function () {
-console.log("Hapi Ready");
+
 	server.route([
 	  { method: 'GET', path: '/', handler: { file: "index.html" } },
 		// switch these two routes for a /static handler?
